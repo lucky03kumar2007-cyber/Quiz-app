7282ea4817bf13e99d31f19e5f5dff8b584d394f
lucky03kumar2007-cyber <lucky.03kumar2007@gmail.com>
2026-09-10 01:24:13 +0530

    Add files via upload

---
+++ backend/questions.js
@@ -0,0 +1,104 @@
+const questions = [
+    {
+        question: "What is the full form of HTML ?",
+        options: [
+            "Hyper Text Markup Language",
+            "High Text Machine Language",
+            "Hyper Tool Multi Language",
+            "Home Text Markup Language"
+        ],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "What is the full form of CSS ?",
+        options: [
+            "Computer Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style System",
+            "Colorful Style Sheets"
+        ],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "What is JavaScript used for ?",
+        options: [
+            "To make the website interactive ",
+            "Just for photo editing",
+            "Just for creating a database",
+            "Just for playing the video"
+        ],
+        answer: "To make the website interactive"
+    },
+    {
+        question: "Which tag is used to create a paragraph in HTML?",
+        options: [
+            "p",
+            "h1",
+            "<para>",
+            "<text>"
+        ],
+        answer:"p"
+    },
+    {
+        question: "Which tag is used to add an image in HTML ?",
+        options: [
+            "<image>",
+            "<img>",
+            "<pic>",
+            "<src>"
+        ],
+        answer:"<img>"
+    },
+    {
+        question: "What is the reason for that tightness?",
+        options: [
+            "To style the website",
+            "To create a database",
+            "To create a server",
+            "To create a file"
+        ],
+        answer:"To style the website"
+    },
+    {
+        question: "Which property is used to change the text color in CSS?",
+        options: [
+            "font-color",
+            "text-color",
+            "color",
+            "text-style"
+        ],
+        answer: "color"
+    },
+    {
+        question:" Which keyword can be used to declare a variable in JavaScript ?",
+        options: [
+            "let",
+            "variable",
+            "varname",
+            "declare"
+        ],
+        answer: "let"
+    },
+    {
+        question:"Which event is used in JavaScript to trigger a function a button is clicked ?",
+        options: [
+            "onhover",
+            "onclick",
+            "onload",
+            "onpress"
+        ],
+        answer: "onclick"
+    },
+    {
+        question: "Which tag is used to create a hyperlink in HTML ?",
+        options: [
+            "<link>",
+            "<a>",
+            "<url>",
+            "<href>"
+        ],
+        answer: "<a>"
+    }
+];
+
+module.exports = questions;
